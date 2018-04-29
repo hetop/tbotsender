25ded60f2384a71cddea392b182ad31c23c08d6c
hetop <38821738+hetop@users.noreply.github.com>
2018-04-29 10:54:00 +0300

    .

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
+    <head lang="ru">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="format-detection" content="telephone=no" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
